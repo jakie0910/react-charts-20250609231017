56b893d1b434caf3d478ddd72af6b1c171581deb
jakie0910 <jakie0910@outlook.com>
2025-06-10 14:11:52 +0800

    Add JavaScript tests

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { reducers } from './reducers/reducers';
+import { createStore } from 'redux';
+
+export function configureStore(initialState = {}) {
+  const store = createStore(reducers, initialState);
+  return store;
+};
+
+export const store = configureStore();
